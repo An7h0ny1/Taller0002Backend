4eb8a294792189286d14dc8a899000edd7d3c450
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2023-12-09 12:03:55 -0500

    first commit

---
+++ modelos/mascotasModelo.js
@@ -0,0 +1,24 @@
+import Sequelize from "sequelize";
+import { db } from "../database/conexion.js";
+
+
+const mascotas = db.define("mascotas",{
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    nombre: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    edad: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+
+    },
+});
+
+export {mascotas}
+
